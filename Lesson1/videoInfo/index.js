c8d05df181dfb6d38a7e6cc86fe4015b26a0148d
Lauro Cabral <lauro199471@gmail.com>
2019-08-29 00:35:12 -0700

    added bootstrap

--- Lesson1/videoInfo/index.js
+++ Lesson1/videoInfo/index.js
@@ -8,3 +8,8 @@ app.on('ready', () => {
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 });
+
+$('.custom-file-input').on('change',function(){
+    var fileName = document.getElementById("exampleInputFile").files[0].name;
+    $(this).next('.form-control-file').addClass("selected").html(fileName);
+  })
