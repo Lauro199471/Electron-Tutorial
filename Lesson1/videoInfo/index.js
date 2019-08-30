0f3fd06480694b5845a280b5efb5749b50c29cf0
Lauro Cabral <Lauro199471@gmail.com>
2019-08-29 18:22:30 -0700

    added submit button

--- Lesson1/videoInfo/index.js
+++ Lesson1/videoInfo/index.js
@@ -9,7 +9,8 @@ app.on('ready', () => {
 
 });
 
-$('.custom-file-input').on('change',function(){
-    var fileName = document.getElementById("exampleInputFile").files[0].name;
-    $(this).next('.form-control-file').addClass("selected").html(fileName);
-  })
+// Add the following code if you want the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
