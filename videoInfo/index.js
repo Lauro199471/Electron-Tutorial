93fddb392ca871d86ac0b690dfca2e3898347a31
Lauro Cabral <Lauro199471@gmail.com>
2019-08-31 00:19:11 -0700

    barebone start

---
+++ videoInfo/index.js
@@ -0,0 +1,15 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+    console.log('App is now ready');
+    const mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+
---
+++ videoInfo/support.js
@@ -0,0 +1,7 @@
+const electron = require('electron');
+
+// Button Function Handler
+document.querySelector('form').addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+});
