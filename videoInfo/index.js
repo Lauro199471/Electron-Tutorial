d7aac75154509bfe0c1d266c802c30f3871b49f0
Lauro Cabral <lauro199471@gmail.com>
2019-08-31 01:37:01 -0700

    formatted code

--- videoInfo/index.js
+++ videoInfo/index.js
@@ -1,26 +1,26 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow, ipcMain } = electron;
+const {app,BrowserWindow,ipcMain} = electron;
 let mainWindow;
 
 app.on('ready', () => {
-    console.log('App is now ready');
-    mainWindow = new BrowserWindow({
-        webPreferences:{
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+  console.log('App is now ready');
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('video:submit', (event, path) => {
-    // receive info into Electron
-    ffmpeg.ffprobe(path, (err, metadata) => {
-      // send info from electron to main window
-      mainWindow.webContents.send(
-        'video:metadata',
-        metadata.format.duration
-      );
-    });
+  // receive info into Electron
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    // send info from electron to main window
+    mainWindow.webContents.send(
+      'video:metadata',
+      metadata.format.duration
+    );
   });
+});
